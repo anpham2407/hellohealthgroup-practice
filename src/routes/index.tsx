84e8085ef9ecd63f4162d4e04c9b174cd59ac99a
An Pham <an.pham@thuocsi.vn>
2021-06-21 01:10:33 +0700

    practice 2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,25 @@
 import React, { ReactElement } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "../views/home";
+import Practice2 from "../views/practice-2";
 
 const IndexRouter = () => {
   return (
     <Router>
-      <ul>
+      <ul className="navbar">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Practice 1</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/practice-2">Practice 2</Link>
         </li>
       </ul>
       <Switch>
         <Route exact path="/">
           <Home />
+        </Route>
+        <Route exact path="/practice-2">
+          <Practice2 />
         </Route>
       </Switch>
     </Router>
---
+++ src/views/practice-2/index.tsx
@@ -0,0 +1,99 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+import { Pagination } from "./../../components/paging";
+const alignIcon = "../../assets/icons/left-align.svg";
+const plusIcon = "../../assets/icons/plus.svg";
+
+function Home() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const totalPages = Math.round(data.length / 5);
+  const [dataPaging, setDataPaging] = useState([]);
+  const handlePages = (updatePage: number) => setPage(updatePage);
+
+  useEffect(() => {
+    const newData: string[] = [];
+    if (data) {
+      for (let i = page; i < page + 5; i++) {
+        newData.push(data[i - 1]);
+      }
+      setDataPaging(newData);
+    }
+  }, [data, page]);
+
+  useEffect(() => {
+    fetch("https://60ccc69771b73400171f88e7.mockapi.io/api/v1/employees")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setData(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log("error", error);
+        }
+      );
+  }, []);
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="practice-2">
+          <table className="table">
+            <thead className="table-head">
+              <tr className="table-row table-row-head">
+                <th className="table-cell table-cell-head" scope="col">
+                  <i className="icon">Aa</i>
+                  <span>Name</span>
+                </th>
+                <th className="table-cell table-cell-head" scope="col">
+                  <i className="icon">@</i>
+                  Email
+                </th>
+                <th className="table-cell table-cell-head" scope="col">
+                  <i className="icon svg">
+                    <img src={alignIcon} />
+                  </i>
+                  Position
+                </th>
+              </tr>
+            </thead>
+            {dataPaging?.map((item) => (
+              <tbody className="table-body">
+                <tr className="table-row">
+                  <td className="table-cell">{item?.name}</td>
+                  <td className="table-cell">{item?.email}</td>
+                  <td className="table-cell">{item?.position}</td>
+                </tr>
+              </tbody>
+            ))}
+            <tfoot className="table-footer">
+              <tr className="table-row table-row-footer">
+                <td className="table-cell table-cell-footer" colSpan={3}>
+                  <div className="row">
+                    <div className="add-new">
+                      <i className="icon svg">
+                        <img src={plusIcon} />
+                      </i>
+                      <span>New</span>
+                    </div>
+                    <div className="table-paging">
+                      <Pagination
+                        page={page}
+                        totalPages={totalPages}
+                        handlePagination={handlePages}
+                      />
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
