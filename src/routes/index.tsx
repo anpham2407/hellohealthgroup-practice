691b301ebd864e87304a9ee7b79e076efd40fc99
An Pham <an.pham@thuocsi.vn>
2021-06-21 01:24:06 +0700

    rename

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "../views/home";
+import Home from "../views/practice-1";
 import Practice2 from "../views/practice-2";
 
 const IndexRouter = () => {
