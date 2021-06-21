790592f94345facd01fedb63f2da16ec806acce5
An Pham <an.pham@thuocsi.vn>
2021-06-21 16:09:21 +0700

    clean code

--- src/views/practice-2/index.tsx
+++ src/views/practice-2/index.tsx
@@ -28,9 +28,6 @@ function Practice() {
         (result) => {
           setData(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           console.log("error", error);
         }
